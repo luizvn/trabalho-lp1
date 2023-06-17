dbea821b53c599040265b0922e9633c0cbb7a479
Gabrielogui <136906655+Gabrielogui@users.noreply.github.com>
2023-06-17 10:38:46 -0300

    Add files via upload

---
+++ TrabalhoLP1.c
@@ -0,0 +1,240 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> // MANIPULAR STRING
+#include <locale.h> // Biblioteca para deixar em português
+
+// ======= DEFINES ========
+#define MAX_CHAR 40
+#define MAX_TEL 15
+#define MAX_NUM 5
+
+// ======= GLOBAIS ========
+typedef struct Contato{
+    char nome[MAX_CHAR];
+    int dia_nascimento, mes_nascimento, ano_nascimento;
+    char rua[MAX_CHAR], numero[MAX_NUM], bairro[MAX_CHAR], cidade[MAX_CHAR], estado[MAX_CHAR];
+    char numero_telefone[MAX_TEL];
+    struct Contato *prox;
+} contato;
+
+// ======= FUNÇÕES =======
+int Menu_Inicial(int op) // FUNÇÃO DO MENU PRINCIPAL
+{
+    switch(op){
+    case 1: // Contiuar o programa
+        break;
+    case 2:
+        Creditos();
+        break;
+    case 3:
+        printf("Nada ainda...\n");
+        system("pause");
+        break;
+    case 4:
+        printf("Nada ainda...\n");
+        system("pause");
+        break;
+    default:
+        printf("Opção inválida\n");
+        system("pause");
+        break;
+    }
+    system("cls");
+}
+
+void Creditos() // FUNÇÃO CRÉDITOS
+{
+    system("cls");
+    printf("\n ====================CREDITOS===================");
+    printf("\n TRABALHO UNIVERSITÁRIO 2º SEMESTRE             ");
+    printf("\n UNIVERSIDADE DO ESTADO DA BAHIA(UNEB)          ");
+    printf("\n Alunos: Gabriel Cerqueira Santos Rodrigues     ");
+    printf("\n         Luiz Vinícius Pereira de Oliveira Souza");
+    printf("\n Professor: Ernesto Massa                       ");
+    printf("\n Discipina: Linguagem de Programação I(LPI)     ");
+    printf("\n                                                ");
+    printf("\n ===============================================");
+
+    printf("\n\n");
+
+    printf("\n ´´´´´´´´´´´´´´#´´´´´´´´´#´´´´´´´´´#´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´");
+    printf("\n ´´´´´´´´´´´´´´´####´´´´´´####´´´´´#####´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´");
+    printf("\n ´´´´´´´´´´´´´´´´´####´´´´´####´´´´´´####´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´");
+    printf("\n ´´´´´´´´´´´´´´#######´´´####@#´´´#######´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´");
+    printf("\n ´´´´´´´´´´´´´´´#####´´´´´####´´´´´#####´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´");
+    printf("\n ´´´´´´´´´´´´#############################´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´");
+    printf("\n ´´´´´´´´´´´´#############################´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´");
+    printf("\n ´´´´´´´´´´´´#############################´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´###´´´´´");
+    printf("\n ´´´´´´´´´´´´######´´#############´#######´´´´´´´´´´´´´´´´´´´´##´´´´´´´´´##´´´´´##´´´´´´´´##´´´´´#########´´´´´##´´´´###´");
+    printf("\n ´´´´´´´´´´´´####´##´##´##########´´######´´´´´´´´´´´´´´´´´´´´##´´´´´´´´´##´´´´#####´´´´´´##´´´´´#########´´´´´##´´´´´##´");
+    printf("\n ´´´´´´´´´´´´#############################´´´´´´´´´´´´´´´´´´´´##´´´´´´´´´##´´´´##´###´´´´´##´´´´´###´´´´´´´´´´´##´´´´´##´");
+    printf("\n ´´´´´´´´´´´´#####´#´############´´#´#####´´´´´´´´´´´´´´´´´´´´##´´´´´´´´´##´´´´##´´´###´´´##´´´´´#######´´´´´´´#######´´´");
+    printf("\n ´´´´´´´´´´´´##´##########################´´´´´´´´´´´´´´´´´´´´##´´´´´´´´´##´´´´##´´´´###´´##´´´´´#######´´´´´´´##´´´´´##´");
+    printf("\n ´´´´´´´´´´´´##´´######################´##´´´´´´´´´´´´´´´´´´´´##´´´´´´´´´##´´´´##´´´´´´#####´´´´´###´´´´´´´´´´´##´´´´´###");
+    printf("\n ´´´´´´´´´´´´##´´#########################´´´´´´´´´´´´´´´´´´´´###´´´´´´´###´´´´##´´´´´´´####´´´´´##########´´´´##´´´´´###");
+    printf("\n ´´´´´´´´´´´´##´´´´´´´´´´´´##´´´´´´´´´´###´´´´´´´´´´´´´´´´´´´´´###´´´´´###´´´´´##´´´´´´´´###´´´´´##########´´´´#########´");
+    printf("\n ´´´´´´´´´´´´##´´´´´´´´´´´´##´´´´´´´´´´###´´´´´´´´´´´´´´´´´´´´´###´´´´´###´´´´´##´´´´´´´´###´´´´´##########´´´´#########´");
+    printf("\n ´#´´´´#####´##´##########´############´##´#####´´´´#´´´´´´´´´´´´´####´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´");
+    printf("\n ´########################################´########@@´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´");
+    printf("\n ###########´######´´#####´#######´#######´####@##@@@#´´´´´´´#############################´´´############################");
+    printf("\n ´#####´´###´######´´##´##########´´##########´#####´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´");
+    printf("\n ´´´´´´#####´#############################´####´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´");
+    printf("\n ´´´´´#####´´#######´#############´#######´´#@@##´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´");
+    printf("\n ´´´´´##´###´##############´#############´´###@@##´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´");
+    printf("\n ´´´´´#########´#################################´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´");
+    printf("\n ´´´´´´#############´´´´#######´´´´´############´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´");
+    printf("\n ´´´´´´´´#####################################´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´");
+    printf("\n ´´´´´´´´´´#################################´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´");
+    printf("\n ´´´´´´´´´´´´´´##########################´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´");
+    printf("\n ´´´´´´´´´´´´´´´´###´#############´´##´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´");
+    printf("\n ´´´´´´´´´´´´´´´´´##´´´´´´´##´´´´´´´´#´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´");
+    printf("\n ´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´");
+
+    printf("\n\n");
+
+    system("pause");
+}
+
+int Menu_Contatos(int op2)
+{
+    switch(op2){
+    case 1:
+        Adicionar_Contato();
+        break;
+    case 2:
+        Remover_Contato();
+        break;
+    case 3:
+        Exibir_UM();
+        break;
+    case 4:
+        Exibir_Todos();
+        break;
+    case 5:
+        Atualizar_Contato();
+        break;
+    case 6:
+        printf("\n Nada ainda...");
+        getch();
+        break;
+    default:
+        printf("\n Opção inválida...");
+        system("pause");
+    }
+}
+
+void Adicionar_Contato(contato **lista)
+{
+    system("Cls");
+    getchar(); //  para pegar o espaço e não sujar outras variáveis
+
+    contato *novo_contato = (contato *)malloc(sizeof(contato)); // CRIA UMA NOVA STRUCT PARA ADD UM USUARIO
+
+    printf("\n Digite seu nome: ");
+    fgets(novo_contato->nome, MAX_CHAR, stdin);
+    novo_contato->nome[strcspn(novo_contato->nome, "\n")] = '\0';
+
+    printf("\n Digite sua data de nascimento: (dia/mês/ano)\t");
+    scanf("%d/%d/%d", &novo_contato->dia_nascimento, &novo_contato->mes_nascimento, &novo_contato->ano_nascimento);
+    getchar();
+
+    /*printf("%d/%d/%d", novo_contato->dia_nascimento, novo_contato->mes_nascimento, novo_contato->ano_nascimento);
+    getch();*/
+    // --PEDINDO ENDEREÇO--
+    printf("\n DIGITE O ENDEREÇO DO CONTATO: ");
+
+    printf("\n Rua:    ");
+    fgets(novo_contato->rua,MAX_CHAR,stdin);
+    novo_contato->rua[strcspn(novo_contato->rua, "\n")] = '\0';
+
+    printf("\n Número: ");
+    fgets(novo_contato->numero,MAX_CHAR,stdin);
+    novo_contato->numero[strcspn(novo_contato->numero, "\n")] = '\0';
+
+    printf("\n Bairro: ");
+    fgets(novo_contato->bairro,MAX_CHAR,stdin);
+    novo_contato->bairro[strcspn(novo_contato->bairro, "\n")] = '\0';
+
+    printf("\n Cidade: ");
+    fgets(novo_contato->cidade,MAX_CHAR,stdin);
+    novo_contato->cidade[strcspn(novo_contato->cidade, "\n")] = '\0';
+
+    printf("\n Estado: ");
+    fgets(novo_contato->estado,MAX_CHAR,stdin);
+    novo_contato->estado[strcspn(novo_contato->estado, "\n")] = '\0';
+
+    // --------------------
+    novo_contato->prox = *lista;
+    *lista = novo_contato;
+}
+
+void Remover_Contato()
+{
+    system("Cls");
+    printf("\n Remover Contato(NADA AINDA)\n");
+    system("pause");
+}
+
+void Exibir_UM()
+{
+    system("Cls");
+    printf("\n Exibir UM Contato(NADA AINDA)\n");
+    system("pause");
+}
+
+void Exibir_Todos()
+{
+    system("Cls");
+    printf("\n Exibir TODOS os Contato(NADA AINDA)\n");
+    system("pause");
+}
+
+void Atualizar_Contato()
+{
+    system("Cls");
+    printf("\n Atualizar Contato(NADA AINDA)\n");
+    system("pause");
+}
+
+int main(){
+    int op; // Opção do Menu Inicial
+    setlocale(LC_ALL,""); // Deixar em português
+    /*printf("| SISTEMA DE MANIPULAÇÃO DE CONTATOS | \n\n");
+    system("pause");
+    system("cls");*/
+    contato *lista = NULL;
+    do{
+        printf("| SISTEMA DE MANIPULAÇÃO DE CONTATOS | \n\n");
+        system("pause");
+        printf("\n =====================");
+        printf("\n MENU INICIAL:        ");
+        printf("\n =====================");
+        printf("\n 1 - Acessar o sistema");
+        printf("\n 2 - Créditos         ");
+        printf("\n 3 -                  ");
+        printf("\n 4 -                  ");
+        printf("\n =====================");
+        printf("\n ESCOLHA A OPÇÃO:     ");
+        scanf("%d", &op);
+        Menu_Inicial(op);
+    }while(op != 1);
+
+    int op2;
+    while(1){ // While INFINITO
+        system("cls");
+        printf("\n ============================");
+        printf("\n MENU DE CONTATOS:           ");
+        printf("\n ============================");
+        printf("\n 1 - Adicionar Contato       ");
+        printf("\n 2 - Remover Contato         ");
+        printf("\n 3 - Exibir UM Contato       ");
+        printf("\n 4 - Exibir TODOS os contatos");
+        printf("\n 5 - Atualizar Contato       ");
+        printf("\n 6 - Sair do Programa        ");
+        printf("\n ============================");
+        printf("\n ESCOLHA A OPÇÃO:            ");
+        scanf("%d", &op2);
+        Menu_Contatos(op2);
+    }
+    return 0;
+}
